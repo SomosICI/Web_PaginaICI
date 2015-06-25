96aefe95588946b0546f4b3947001eaf5e8cf274
suleymarz <suleyma_rz@hotmail.com>
2015-06-25 13:54:09 -0500

    Diseño Inicial

---
+++ index.php
@@ -0,0 +1,41 @@
+<!doctype html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+		<title>Sitio web Empresa JADY</title>
+		<style type="text/css"></style>
+		<link rel="stylesheet" href="css/estilo.css">
+	</head>
+
+	<body>
+		<header>
+			<div id="subheader">
+                <div id="logo"><img src="images/Logo.jpg" width="130"></div>
+                <nav>
+                <a href="#">SIGN OUT</a>
+                </nav>
+			</div>
+            <ul class="menu">
+                
+                <li><a href="#" >Inventario</a></li>
+                <li><a href="#">Proveedores</a></li>
+                <li><a href="#">Ventas</a></li>
+                <li><a href="#">Compras</a></li>
+                <li><a href="#">Usuarios</a></li>
+            </ul>
+		</header>
+        <div class="cuerpo">
+        <table border=0>
+                <td>0 </td>
+                <tr class="azul"><td>1 </td></tr>
+                <tr><td>2 </td></tr>
+                <tr class="azul"><td>3</td></tr>
+                <tr><td>4</td></tr>
+                <tr class="azul"><td>5 </td></tr>
+                <tr><td>6 </td></tr>
+                <tr class="azul"><td>7 </td></tr>
+                <tr><td>8 </td></tr>
+                <tr class="azul"><td>9 </td></tr> 
+            </table>
+	</body>
+<html>
